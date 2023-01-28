432fab18c952f7c257867576cdde5bdb8e1c1d39
Animesh Srivastav <97078697+AnimeshSrivastav@users.noreply.github.com>
2023-01-28 15:55:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-unused-vars */
+const functions = require("firebase-functions");
+const express = require("express");
+const cors = require("cors");
+const request = require("request");
+const app = express();
+
+const apikey = "a4f3f7cd74484a9623f61a5b2f95e10e";
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.get("/getweather/:city", (req, res) => {
+  const city = req.params.city;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+  request(url, (err, response, body) => {
+    if (!err) {
+      res.send(JSON.parse(body));
+    }
+  });
+});
+
+
+exports.app = functions.https.onRequest(app);
